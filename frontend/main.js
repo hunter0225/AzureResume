83de28306eacf2771981df641731c428c7b28e14
hunter0225 <hunterscott0225@gmail.com>
2024-02-29 20:50:58 -0500

    updates

--- frontend/main.js
+++ frontend/main.js
@@ -1,12 +1,12 @@
 window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
 })
-
-const functionApi = 'http://localhost:7071/api/GetResumeCounter';
+const functionApiUrl = 'https://func-resume-hscott.azurewebsites.net/api/GetResumeCounter?code=kB5GD2n-lfW-QvhcX4lrE0NfgrTleQDNe0DuuaHH1_V0AzFu39DtPQ==';
+const localfunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi).then(response => {
+    fetch(functionApiUrl).then(response => {
         return response.json()
     }).then(response =>{
         console.log("Website called function API");
